dd54fd683412b70d39ff26b2a172eb698314c5ca
Chestnut <li1928631@163.com>
2023-07-22 21:31:35 +0800

    修改补充注释

--- spring-context/src/main/java/com/chestnut/spring/io/InputStreamCallback.java
+++ spring-context/src/main/java/com/chestnut/spring/io/InputStreamCallback.java
@@ -6,13 +6,13 @@ import java.io.InputStream;
 /**
  * 输入流回调接口，用于在处理输入流时执行特定操作，并返回结果
  *
- * @author: Chestnut
- * @since: 2023-07-13
- **/
+ * @param <T> 操作结果类型
+ */
 @FunctionalInterface
 public interface InputStreamCallback<T> {
     /**
      * 在给定的输入流上执行特定操作，并返回结果
+     *
      * @param inputStream 输入流
      * @return 操作结果
      * @throws IOException 如果在操作过程中发生I/O错误
